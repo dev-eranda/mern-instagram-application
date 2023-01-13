e01bc6af9be55fec2bbcd5edecfb9d6f4c4e2054
Eranda_Madusanka <semsamarasinghe@gmail.com>
2023-01-13 14:44:27 +0530

    UI fixed, Home

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const Home = ()=> {
-    return(
-        <h1>Home</h1>
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="card home-card">
+                <h5>John Doe</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{ color: "red" }}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is Post</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>John Doe</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{ color: "red" }}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is Post</p>
+                    <input type="text" placeholder="add a comment"></input>
+                </div>
+            </div>
+        </div>
     )
 }
 
