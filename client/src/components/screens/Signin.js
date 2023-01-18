d735973f7351fbe2d24e82ccef8cd0e36d31ed0c
Eranda_Madusanka <semsamarasinghe@gmail.com>
2023-01-18 09:48:01 +0530

    signin with api

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import M from 'materialize-css'
 
 const Signin = () => {
+
+    const history = useHistory()
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+
+    const PostData = () => {
+
+        if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return M.toast({ html: "invalid email", classes: "#c62828 red darken-3" })
+        }
+
+        fetch("/singin", {
+            method: "post",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }).then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (data.error) {
+                M.toast({ html: data.error, classes: "#c62828 red darken-3" })
+            }
+            else {
+                M.toast({ html: "Success", classes: "#43a047 green darken-1" })
+                history.push('/')
+            }
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="mycard">
             <div className="card auth-card input-field">
@@ -8,12 +45,18 @@ const Signin = () => {
                 <input
                     type="text"
                     placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                    onClick={() => PostData()}
+                >
                     Sign in
                 </button>
                 <h5>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,66 +4,67 @@ const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const jwt = require('jsonwebtoken')
-const {JWT_SECRET} = require('../keys')
+const { JWT_SECRET } = require('../keys')
 
 router.post('/singup', (req, res) => {
-    const {name, email, password} = req.body
-    if(!name || !email || !password){
-        return res.status(422).json({error: "plase add all the fiedls"})
-    }   
-    User.findOne({email: email})
-    .then((savedUser) => {
-        if(savedUser){
-            return res.status(422).json({error: "user already exist with that email"})
-        }
-        bcrypt.hash(password, 12) 
-        .then(hashedpassword => {
-            const user = new User({
-                name,
-                email,
-                password: hashedpassword
-            })
-            user.save()
-            .then(user => {
-                res.json({ message: "saved successfully"})
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        })
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
-
-router.post('/singin', (req, res) => {
-    const {email, password} = req.body
-    if(!email || !password){
-        return res.status(422).json({ error: "please add email or password"})
-    } 
-    User.findOne({email: email})
-    .then(savedUser => {
-        if(!savedUser){
-            return res.status(422).json({ error: "invalid email or password"})
-        }
-        bcrypt.compare(password, savedUser.password)
-        .then(match => {
-            if(match){
-                const token = jwt.sign({ _id: savedUser._id}, JWT_SECRET)
-                res.json({token})
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        return res.status(422).json({ error: "plase add all the fiedls" })
+    }
+    User.findOne({ email: email })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: "user already exist with that email" })
             }
-            else{
-                return res.status(422).json({ error: "invalid email or password"})
-            }
+            bcrypt.hash(password, 12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        name,
+                        email,
+                        password: hashedpassword
+                    })
+                    user.save()
+                        .then(user => {
+                            res.json({ message: "signup successfully" })
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                })
         })
         .catch(error => {
             console.log(error)
         })
-    })
-    .catch(error => {
-        console.log(error)
-    })
+})
+
+router.post('/singin', (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(422).json({ error: "please add email or password" })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "invalid email or password" })
+            }
+            bcrypt.compare(password, savedUser.password)
+                .then(match => {
+                    if (match) {
+                        const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET)
+                        const { _id, name, email } = savedUser
+                        res.json({ token, user: { _id, name, email }})
+                    }
+                    else {
+                        return res.status(422).json({ error: "invalid email or password" })
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 module.exports = router
