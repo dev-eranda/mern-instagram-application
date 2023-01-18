d7ec147dd6af8adedc776ae6fae09a9219dbca5c
Eranda_Madusanka <semsamarasinghe@gmail.com>
2023-01-18 13:39:38 +0530

    changes

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -5,17 +5,16 @@ const User = mongoose.model("User")
 
 module.exports = (req, res, next) => {
     const {authorization} = req.headers
-
-    //authorization === bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2JkMmI1ZWRmOTZkYzYyMjZiODJlNzgi 
-
+    //authorization === bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2JkMmI1ZWRmOTZkYzYyMjZiODJlNzgi
+     
     if(!authorization){
-        return res.status(401).json({ error: "You must be logged in"})
+        return res.status(401).json({error: "You must be logged in"})
     }
 
     const token = authorization.replace("Bearer ", "")
     jwt.verify(token, JWT_SECRET, (error, payload) => {
         if(error){
-            return res.status(401).json({ error: "You must be logged in"})
+            return res.status(401).json({error: "You must be logged in"})
         }
         const {_id} = payload
 
