ac4b0fbfdf3db99a9a44cbbd99cba167d694a737
Eranda_Madusanka <semsamarasinghe@gmail.com>
2023-02-08 13:12:58 +0530

    Map all post to Home screen

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,34 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('/allpost', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+        .then(result => {
+            setData(result.posts)
+        }) 
+    }, [])
+
     return (
         <div className="home">
-            <div className="card home-card">
-                <h5>John Doe</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                        alt="" />
+            {data.map(item => {
+                return(
+                <div className="card home-card" key={item._id}>
+                    <h5>{item.postedBy.name}</h5>
+                    <div className="card-image">
+                        <img src={item.photo}
+                            alt="" />
+                    </div>
+                    <div className="card-content">
+                        <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                        <h6>{item.title}</h6>
+                        <p>{item.body}</p>
+                        <input type="text" placeholder="add a comment"></input>
+                    </div>
                 </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{ color: "red" }}>favorite</i>
-                    <h6>title</h6>
-                    <p>this is Post</p>
-                    <input type="text" placeholder="add a comment"></input>
-                </div>
-            </div>
-            <div className="card home-card">
-                <h5>John Doe</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
-                        alt="" />
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{ color: "red" }}>favorite</i>
-                    <h6>title</h6>
-                    <p>this is Post</p>
-                    <input type="text" placeholder="add a comment"></input>
-                </div>
-            </div>
+                )
+            })}
         </div>
     )
 }
