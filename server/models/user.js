8d811e66ed80f264f57c1acf930e706c51c28957
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-07-18 00:26:15 +0530

    fix: signup rout

--- server/models/user.js
+++ server/models/user.js
@@ -3,15 +3,20 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required'],
+        minlength: [3, 'Name must be at least 3 characters long'],
+        maxlength: [50, 'Name cannot be more than 50 characters long']
     },
     email: {
         type: String,
-        required: true
+        required: [true, 'Email is required'],
+        match: [/\S+@\S+\.\S+/, 'Please provide a valid email address'],
+        unique: true 
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long']
     }
 })
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,38 +6,39 @@ const User = mongoose.model('User')
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../keys')
 
-router.post('/singup', (req, res) => {
+
+router.post('/signup', async(req, res) => {
     const { name, email, password } = req.body
-    if (!name || !email || !password) {
-        return res.status(422).json({ error: "plase add all the fiedls" })
+
+    try{ 
+        const savedUser = await User.findOne({ email: email})
+        if(savedUser){
+            return res.status(422).json({ error: "User alreasy exists with that email"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({
+            name, 
+            email, 
+            password
+        })
+
+        await user.save()
+        return res.status(201).json({message: "Siginup successful"})
     }
-    User.findOne({ email: email })
-        .then((savedUser) => {
-            if (savedUser) {
-                return res.status(422).json({ error: "user already exist with that email" })
-            }
-            bcrypt.hash(password, 12)
-                .then(hashedpassword => {
-                    const user = new User({
-                        name,
-                        email,
-                        password: hashedpassword
-                    })
-                    user.save()
-                        .then(user => {
-                            res.json({ message: "signup successfully" })
-                        })
-                        .catch(error => {
-                            console.log(error)
-                        })
-                })
-        })
-        .catch(error => {
-            console.log(error)
-        })
+    catch(error){
+        console.error(error);
+
+        if (error.name === "ValidationError") {
+            const errors = Object.values(error.errors).map(err => err.message);
+            return res.status(400).json({ error: errors });
+        }
+
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
-router.post('/singin', (req, res) => {
+router.post('/signin', (req, res) => {
     const { email, password } = req.body
     if (!email || !password) {
         return res.status(422).json({ error: "please add email or password" })
@@ -52,7 +53,7 @@ router.post('/singin', (req, res) => {
                     if (match) {
                         const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET)
                         const { _id, name, email } = savedUser
-                        res.json({ token, user: { _id, name, email }})
+                        res.json({ token, user: { _id, name, email } })
                     }
                     else {
                         return res.status(422).json({ error: "invalid email or password" })
@@ -63,7 +64,13 @@ router.post('/singin', (req, res) => {
                 })
         })
         .catch(error => {
-            console.log(error)
+            // console.log(error)
+            // if(error.name === "ValidationError"){
+            //     const error = object.value(error.errors.map(err => err.message))
+            //     console.log(error)
+            //     return res.status(400).json({error})
+            // }
+            // res.status(500).send('500 Internal server error')
         })
 })
 
