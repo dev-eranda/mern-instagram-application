ac28752f221d6eb71dee14d31d266d49fdfc5a0f
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-07-18 02:11:34 +0530

    fic: createpost & mypost

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,62 +2,80 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const Post = mongoose.model('Post')
-const requireLogin = require('../middleware/requireLogin') 
+const requireLogin = require('../middleware/requireLogin')
+const joi = require('joi')
 
 
-router.get('/allpost', requireLogin, (req, res) => {
-    Post.find()
-    .populate("postedBy", "_id name")
-    .populate("comments.commentedBy", "_id name")
-    .then(posts => {
-        res.json({ posts })
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
-
-router.post('/createpost', requireLogin, (req, res) => {
-    const {title, body, photoURI} = req.body
-    if(!title || !body || !photoURI) {
-        return res.status(422).json({error: "plase add all the fiedls"})
+router.get('/allpost', requireLogin, async (req, res) => {
+    try {
+        const post = await Post.find()
+            .populate("postedBy", "_id name")
+            .populate("comments.commentedBy", "_id name")
+        return res.status(200).json({ post })
     }
-    req.user.password = undefined
-    const post = new Post ({
-        title,
-        body,
-        photo: photoURI,
-        postedBy: req.user
-    })
-    post.save().then(result => {
-        res.json({ post: result })
-    })
-    .catch(error => {
-        console.log(error)
-    }) 
+    catch (error) {
+        console.log("cannot find all post:", error)
+        return res.status(500).send("500 Internal server error")
+    }
 })
 
-router.get('/mypost', requireLogin, (req, res) => {
-    Post.find({ postedBy: req.user._id })
-    .populate("postedBy", "_id name")
-    .then( mypost => {
-        res.json({ mypost })
-    })
-    .catch( error => {
+const createpostSchema = joi.object({
+    title: joi.string().min(1).max(50).required(),
+    body: joi.string().min(1).max(100).required(),
+    image_url: joi.string().required()
+})
+
+router.post('/createpost', requireLogin, async (req, res) => {
+    const { error, value } = createpostSchema.validate(req.body)
+    if (error) {
+        return res.status(422).json({ error: error.details[0].message })
+    }
+
+    try {
+        const { title, body, image_url } = value
+        const user = req.user
+        user.password = undefined
+
+        const postObj = new Post({
+            title,
+            body,
+            photo: image_url,
+            postedBy: user
+        })
+
+        const post = await postObj.save()
+        return res.status(200).json({ post })
+    }
+    catch (error) {
         console.log(error)
-    })
+        return res.status(500).send("500 Internal server error")
+    }
+})
+
+router.get('/mypost', requireLogin, async (req, res) => {
+    const { _id } = req.user
+
+    try {
+        const myPost = await Post.find({ postedBy: _id }).populate("postedBy", "_id name")
+        return res.status(200).json({ post: myPost })
+
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).send("500 Internal server error")
+    }
 })
 
 router.put('/like', requireLogin, (req, res) => {
     Post.findByIdAndUpdate(req.body.postId, {
-        $push: {likes: req.user._id}
+        $push: { likes: req.user._id }
     }, {
         new: true
     }).exec((error, result) => {
-        if(error){
-            return res.status(422).json({error: error})
+        if (error) {
+            return res.status(422).json({ error: error })
         }
-        else{
+        else {
             res.json(result)
         }
     })
@@ -65,14 +83,14 @@ router.put('/like', requireLogin, (req, res) => {
 
 router.put('/unlike', requireLogin, (req, res) => {
     Post.findByIdAndUpdate(req.body.postId, {
-        $pull: {likes: req.user._id}
+        $pull: { likes: req.user._id }
     }, {
         new: true
     }).exec((error, result) => {
-        if(error){
-            return res.status(422).json({error: error})
+        if (error) {
+            return res.status(422).json({ error: error })
         }
-        else{
+        else {
             res.json(result)
         }
     })
@@ -86,38 +104,38 @@ router.put('/comment', requireLogin, (req, res) => {
     }
 
     Post.findByIdAndUpdate(req.body.postId, {
-        $push: {comments: comment}
+        $push: { comments: comment }
     }, {
         new: true
     })
-    .populate("postedBy", "_id name")
-    .populate("comments.commentedBy", "_id name")
-    .exec((error, result) => {
-        if(error){
-            return res.status(422).json({error: error})
-        }
-        else{
-            res.json(result)
-        }
-    })
+        .populate("postedBy", "_id name")
+        .populate("comments.commentedBy", "_id name")
+        .exec((error, result) => {
+            if (error) {
+                return res.status(422).json({ error: error })
+            }
+            else {
+                res.json(result)
+            }
+        })
 })
 
 router.delete('/deletepost/:postId', requireLogin, (req, res) => {
-    Post.findOne({_id:req.params.postId})
-    .populate("postedBy", "_id")
-    .exec((error, post) => {
-        if(error || !post){
-            return res.json(422).json({error: error})
-        }
-        if(post.postedBy._id.toString() === req.user._id.toString()){
-            post.remove()
-            .then(result => {
-                res.json({result})
-            }).catch(error => {
-                console.log(error)
-            })
-        }
-    })
+    Post.findOne({ _id: req.params.postId })
+        .populate("postedBy", "_id")
+        .exec((error, post) => {
+            if (error || !post) {
+                return res.json(422).json({ error: error })
+            }
+            if (post.postedBy._id.toString() === req.user._id.toString()) {
+                post.remove()
+                    .then(result => {
+                        res.json({ result })
+                    }).catch(error => {
+                        console.log(error)
+                    })
+            }
+        })
 })
 
 module.exports = router
