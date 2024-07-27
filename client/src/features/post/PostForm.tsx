81f019fac8fc26bde6e45ece06d3ce2beea3543a
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-07-27 13:09:24 +0530

    fix: create post

--- client/src/features/post/PostForm.tsx
+++ client/src/features/post/PostForm.tsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import { Input } from "../../components/ui/Input";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Button } from "../../components/ui/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
 import { Post } from "../../types/post";
+import { useSelector, UseSelector } from "react-redux";
+import z from "zod";
+import { RootState } from "../../types";
+
+const MAX_FILE_SIZE = 1024 * 1024 * 2;
+const ACCEPTED_IMAGE_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
 
 const formSchema = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().min(1, "Description is required"),
-  // file: z
-  //   .instanceof(File)
-  //   .refine(
-  //     (file) => {
-  //       if (!file) return false;
-  //       const allowedTypes = ["image/jpeg", "image/png"];
-  //       return allowedTypes.includes(file.type);
-  //     },
-  //     {
-  //       message: "File must be of type .jpg or .png",
-  //     }
-  //   )
-  //   .nullable(),
+  file: z
+    .any()
+    .refine((file) => file && file.length > 0, "Document is required.")
+    .refine((file) => {
+      return file?.[0]?.size <= MAX_FILE_SIZE;
+    }, `Max document size is 2MB.`)
+    .refine(
+      (file) => ACCEPTED_IMAGE_MIME_TYPES.includes(file?.[0]?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported."
+    ),
 });
 
 type FormFields = z.infer<typeof formSchema>;
@@ -34,21 +41,72 @@ const PostForm = ({ post }: postFormProps) => {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors, isSubmitting },
   } = useForm<FormFields>({
     defaultValues: post
       ? {
           title: post.title,
           description: post.body,
-          // file: post.photo, // Handle this properly if needed
+          file: post.photo,
         }
       : undefined,
     resolver: zodResolver(formSchema),
   });
+  const [url, setUrl] = useState<string>("");
+  const { token } = useSelector((state: RootState) => state.auth);
+
+  const postDetails = async (file: File): Promise<string> => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "insta-clone");
+    data.append("cloud_name", "dgbgvecx3");
+
+    const response = await fetch(
+      "https://api.cloudinary.com/v1_1/dgbgvecx3/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const result = await response.json();
+    if (result.error) {
+      throw new Error(result.error.message);
+    }
+    return result.url;
+  };
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    console.log("Form submitted");
-    console.log(data);
+    const title = data.title;
+    const body = data.description;
+    const file = data.file;
+
+    const imageUrl = await postDetails(file[0]);
+    setUrl(imageUrl);
+
+    try {
+      const response = await fetch("/post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ title, body, image_url: url }),
+      });
+
+      const result = await response.json();
+      if (result.error) {
+        throw new Error(result.error);
+      }
+      alert("success");
+    } catch (error) {
+      if (error instanceof Error) {
+        setError("root", {
+          message: error.message || "An unexpected error occurred",
+        });
+      }
+    }
   };
 
   return (
@@ -68,13 +126,13 @@ const PostForm = ({ post }: postFormProps) => {
           register={register}
           error={errors.description?.message}
         />
-        {/* <Input
+        <Input
           label="Image"
           name="file"
           type="file"
           register={register}
-          error={errors.file?.message}
-        /> */}
+          error={errors.file?.message?.toString()}
+        />
         <Button disabled={isSubmitting}>
           {isSubmitting ? "Posting..." : "Post"}
         </Button>
@@ -83,7 +141,6 @@ const PostForm = ({ post }: postFormProps) => {
             <span className="input-error">{errors.root?.message}</span>
           )}
         </div>
-        {errors && <pre>{JSON.stringify(errors, null, 2)}</pre>}
       </form>
     </Layout>
   );
