0e09006807caf7c1fb73071be3d6a1b550af7718
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-09-10 15:32:34 +0530

    fix api in frontend

--- client/src/api/axios.tsx
+++ client/src/api/axios.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 // const BASE_URL = "http://192.168.8.124:5000";
-const BASE_URL = "https://16.171.34.240/api";
+const BASE_URL = process.env.NODE_ENV === "production" ? "https://16.171.34.240/api" : "http://localhost:5000/api";
+
+console.log(BASE_URL)
 
 export default axios.create({
   baseURL: BASE_URL,
