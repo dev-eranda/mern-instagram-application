36ee38ec314a589017cf9d0057b03eed633525b6
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-20 21:42:14 +0530

    chnages

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -1,41 +1,40 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 // const mongoose = require('mongoose')
 // const User = mongoose.model("User")
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = async (req, res, next) => {
-    const { authorization } = req.headers
+   const { authorization } = req.headers;
 
-    if (!authorization) {
-        return res.status(401).json({ error: "You must be logged in" })
-    }
+   if (!authorization) {
+      return res.status(401).json({ error: "You must be logged in" });
+   }
 
-    // const token = authorization.replace("Bearer ", "")
-    const token = authorization.split(" ")[1]
+   // const token = authorization.replace("Bearer ", "")
+   const token = authorization.split(" ")[1];
 
-    try {
-        const payload = jwt.verify(token, JWT_SECRET, (error, user) => {
-            if (error) {
-                console.log(error)
-                return res.status(403).json("Token is not valid!")
-            }
-            req.user = user;
-            next();
-        })
+   try {
+      const payload = jwt.verify(token, JWT_SECRET, (error, user) => {
+         if (error) {
+            console.log(error);
+            return res.status(403).json("Token is not valid!");
+         }
+         req.user = user;
+         next();
+      });
 
-        // const { _id } = payload
+      // const { _id } = payload
 
-        // const currentUser = await User.findById(_id);
-        // if (!currentUser) {
-        //     return res.status(401).json({ error: 'User not found' });
-        // }
+      // const currentUser = await User.findById(_id);
+      // if (!currentUser) {
+      //     return res.status(401).json({ error: 'User not found' });
+      // }
 
-        // req.user = currentUser;
-        // next();
-    }
-    catch (error) {
-        console.error('Error verifying token or finding user:', error);
-        return res.status(401).json({ error: "You must be logged in" })
-    }
-} 
+      // req.user = currentUser;
+      // next();
+   } catch (error) {
+      console.error("Error verifying token or finding user:", error);
+      return res.status(401).json({ error: "You must be logged in" });
+   }
+};
